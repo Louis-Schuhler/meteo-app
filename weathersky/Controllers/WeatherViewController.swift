eaad317d14c5da2771601ae81a289201ed950b9a
Louis Schuhler 🤿 <148199476+Louis-Schuhler@users.noreply.github.com>
2024-05-15 09:34:30 +0200

    Create WeatherViewController.swift

---
+++ weathersky/Controllers/WeatherViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
