d0b446625f7be830816ad6e92c39a0d101f8678f
Louis Schuhler 🤿 <148199476+Louis-Schuhler@users.noreply.github.com>
2024-05-15 09:32:06 +0200

    Create WeatherManager.swift

---
+++ weathersky/Managers/WeatherManager.swift
@@ -0,0 +1,13 @@
+import Foundation
+import WeatherKit
+import CoreLocation
+
+final class WeatherManager{
+    static let shared = WeatherManager();
+    
+    private init(){}
+    
+    public func getWeather(for location: CLLocation){
+        
+    }
+}
